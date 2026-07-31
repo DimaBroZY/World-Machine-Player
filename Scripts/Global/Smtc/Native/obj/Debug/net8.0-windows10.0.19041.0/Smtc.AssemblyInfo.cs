e8ea14b2f5a5a25f0c6f9a722f595661590949c9
DimaBroZY <ershovdmitrie@gmail.com>
2026-08-01 00:17:07 +0500

    idk

--- Scripts/Global/Smtc/Native/obj/Debug/net8.0-windows10.0.19041.0/Smtc.AssemblyInfo.cs
+++ Scripts/Global/Smtc/Native/obj/Debug/net8.0-windows10.0.19041.0/Smtc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72ff121d7c18cb848df24b2d6b75acd153c2b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d54e1e16753da6518c77d52721d90382bde836")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
