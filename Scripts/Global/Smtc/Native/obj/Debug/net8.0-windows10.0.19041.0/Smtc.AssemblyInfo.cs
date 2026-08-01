3b4903caf6d86740e70001f6f91b1ee01d9ff2e6
DimaBroZY <ershovdmitrie@gmail.com>
2026-08-02 04:12:25 +0500

    fixed border color in playlist scene

--- Scripts/Global/Smtc/Native/obj/Debug/net8.0-windows10.0.19041.0/Smtc.AssemblyInfo.cs
+++ Scripts/Global/Smtc/Native/obj/Debug/net8.0-windows10.0.19041.0/Smtc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2133478089b2832e0c5d537971a4aabbd20e0839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bff455d3d9dc0b4543ffe62fd2f792000345667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smtc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
